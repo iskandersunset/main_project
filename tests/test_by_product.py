658a5531ce3bdb4b3be1fea83a8f8109b4044c91
Александр Халезин <iskandersunset@gmail.com>
2022-10-20 15:24:50 +0500

    Добавили скрин последней страницы

--- tests/test_by_product.py
+++ tests/test_by_product.py
@@ -4,8 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from pages.cart_page import Cart_page
 from pages.client_information_page import Client_information_page
+from pages.finish_page import Finish_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
+from pages.payment_page import Payment_page
 
 
 def test_by_product():
@@ -28,5 +30,12 @@ def test_by_product():
     # Заполняем данные и жмем Continue
     cip = Client_information_page(driver)
     cip.input_information()
+
+    # Жмем финиш
+    p = Payment_page(driver)
+    p.payment()
+
+    s = Finish_page(driver)
+    s.get_screenshot()
 
     time.sleep(2)
