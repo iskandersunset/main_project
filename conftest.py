d0e463986577d140c3fc805dc6bad6dc141bca8e
Александр Халезин <iskandersunset@gmail.com>
2022-10-20 18:52:25 +0500

    Release

--- conftest.py
+++ conftest.py
@@ -6,3 +6,11 @@ def set_up():
     print("Start TEST")
     yield
     print("Finish TEST")
+
+
+@pytest.fixture(scope="module")
+def set_group():
+    print("Enter System")
+    yield
+    print("Exit System")
+
