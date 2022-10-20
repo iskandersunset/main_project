4e2efaf28baf95cfd7dd9cdbe49051a4a9f32fde
Александр Халезин <iskandersunset@gmail.com>
2022-10-20 10:23:26 +0500

    Добавили методо get_url

--- base/base_class.py
+++ base/base_class.py
@@ -1,6 +1,13 @@
-
-
 class Base():
 
     def __init__(self, driver):
         self.driver = driver
+
+    """Method Get current URL"""
+
+    def get_current_rl(self):
+        get_url = self.driver.current_url
+        print("Curent URL " + get_url)
+
+    """Method Get current URL"""
+
