78cb69386364c29d0c1157240646ca353bbb5b77
Александр Халезин <iskandersunset@gmail.com>
2022-10-20 12:41:02 +0500

    init

---
+++ pages/cart_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from base.base_class import Base
+
+
+class Cart_page(Base):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators============================
+
+    checkout_button = "checkout"
+
+    # Getters=============================
+
+    def get_checkout_button(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, self.checkout_button)))
+
+    # Actions =============================
+    def click_checkout_button(self):
+        self.get_checkout_button().click()
+        print("Click add to cart product_1")
+
+    # Methods =============================
+    def select_product(self):
+        self.get_current_url()
+        self.click_checkout_button()
