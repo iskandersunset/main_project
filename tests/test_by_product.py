96c52bc0260f21d6183dc31733e3ffb9d6d2342a
Александр Халезин <iskandersunset@gmail.com>
2022-10-20 17:16:36 +0500

    Добавили тесты еще для 2-х товаров и порядок прохождения тестов (не забываем устанавливать pytest-ordering)

--- tests/test_by_product.py
+++ tests/test_by_product.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest as pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from pages.cart_page import Cart_page
@@ -10,32 +11,78 @@ from pages.main_page import Main_page
 from pages.payment_page import Payment_page
 
 
-def test_by_product():
+@pytest.mark.run(order=1)
+def test_by_product_1():
     s = Service('C:/_teach/resource/chromedriver.exe')  # Путь на работе
     driver = webdriver.Chrome(service=s)
 
-    print('START TEST')
+    print('START TEST 1')
 
     login = Login_page(driver)
     login.authorization()
 
     # Выбираем товар и кликаем по корзине
     mp = Main_page(driver)
-    mp.select_product()
+    mp.select_products_1()
 
     # Жмем кнопку Checkout
     cp = Cart_page(driver)
     cp.product_confirmation()
 
-    # Заполняем данные и жмем Continue
-    cip = Client_information_page(driver)
-    cip.input_information()
+    # # Заполняем данные и жмем Continue
+    # cip = Client_information_page(driver)
+    # cip.input_information()
+    #
+    # # Жмем финиш
+    # p = Payment_page(driver)
+    # p.payment()
 
-    # Жмем финиш
-    p = Payment_page(driver)
-    p.payment()
+    # s = Finish_page(driver)
+    # s.finish()
 
-    s = Finish_page(driver)
-    s.finish()
+    print('FINISH TEST 1')
+    time.sleep(2)
 
+
+@pytest.mark.run(order=3)
+def test_by_product_2():
+    s = Service('C:/_teach/resource/chromedriver.exe')  # Путь на работе
+    driver = webdriver.Chrome(service=s)
+
+    print('START TEST 2')
+
+    login = Login_page(driver)
+    login.authorization()
+
+    # Выбираем товар и кликаем по корзине
+    mp = Main_page(driver)
+    mp.select_products_2()
+
+    # Жмем кнопку Checkout
+    cp = Cart_page(driver)
+    cp.product_confirmation()
+
+    print('FINISH TEST 2')
+    time.sleep(2)
+
+
+@pytest.mark.run(order=2)
+def test_by_product_3():
+    s = Service('C:/_teach/resource/chromedriver.exe')  # Путь на работе
+    driver = webdriver.Chrome(service=s)
+
+    print('START TEST 3')
+
+    login = Login_page(driver)
+    login.authorization()
+
+    # Выбираем товар и кликаем по корзине
+    mp = Main_page(driver)
+    mp.select_products_3()
+
+    # Жмем кнопку Checkout
+    cp = Cart_page(driver)
+    cp.product_confirmation()
+
+    print('FINISH TEST 3')
     time.sleep(2)
