297ea1a84c67e75c6d39b114dc5715659988bfbc
Александр Халезин <iskandersunset@gmail.com>
2022-10-20 09:27:52 +0500

    Переделали вместо Ентер кнопка логин

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,11 +5,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from base.base_class import Base
 
-class Login_page:
+
+class Login_page(Base):
+
+    base_url = 'https://www.saucedemo.com/'
 
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
+
+        # Locators
+
+    user_name = "user-name"
+    password = "password"
 
     def authorization(self, login_name, login_password):
         # 1. Авторизация на сайте
@@ -23,7 +33,8 @@ class Login_page:
         print("Ввели пароль")
         time.sleep(1)
 
-        user_pass.send_keys(Keys.RETURN)
-        print('Нажали Еnter\n', '--' * 20)
+        login_button = WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable((By.ID, "login-button")))
+        login_button.click()
+        print('Клик по кнопке логин\n', '--' * 20)
 
 
